795fdcd27bc6be83b0bad02228ac529420904ca7
Meera Shah <meera_kanata@yahoo.ca>
2022-09-10 11:45:57 -0400

    added most content to site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id="heroine" src="./Images/Heroine.png" alt="Hedona portrait" />
+      <div className="main">
+        <div className="streamerLinks">
+          <div className="listenNow">LISTEN NOW:</div>
+          <img className="streamerLinks__icon" src="./Images/Spotify_Icon.png" alt="Spotify" />
+          <img className="streamerLinks__icon" src="./Images/Apple_Icon.png" alt="Apple Music" />
+          <img className="streamerLinks__icon" src="./Images/YT_Icon.png" alt="YouTube Music" />
+          <img className="streamerLinks__icon" src="./Images/Amazon_Icon.png" alt="Amazon Music" />
+          <img className="streamerLinks__icon" src="./Images/Soundcloud_Icon.png" alt="Soundcloud" />
+          <img className="streamerLinks__icon" src="./Images/Tidal_Icon.png" alt="Tidal" />
+        
+        
+        
+        </div>
+        <div className="bodyCopy">
+          <div className="bio">HEDONA crafts moody songs that question genres and push boundaries. Blending sounds and sonic textures that come from an eclectic mix of influences that include Björk, Kanye, YELLE, Sneaker Pimps, Portishead, Radiohead, and beyond, she’s successfully constructed a sound that’s distinctly HEDONA.
+            <p>Her first single, SWIMMING, is available to stream now.</p>
+          </div>
+          <div className="signup">GET THE LATEST NEWS, EXCLUSIVES, AND MORE:
+            <div className="signup__form">
+
+            </div>
+          </div>
+          <div className="socials">
+            <img className="socials__icon" src="./Images/TikTok_Icon.png" alt="TikTok" />
+            <img className="socials__icon" src="./Images/Instagram_Icon.png" alt="Instagram" />
+            <img className="socials__icon" src="./Images/TikTok_Icon.png" alt="LinkedIn" />
+            <img className="socials__icon" src="./Images/Facebook_Icon.png" alt="Facebook" />
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
