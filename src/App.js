08c42d911cb520c5960c0286835b7c9997388b6d
Meera Shah <meera_kanata@yahoo.ca>
2022-09-14 15:03:59 -0400

    added signup form and some links

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import React from "react";
 import './App.css';
+import Mailchimp from "react-mailchimp-form";
 
 function App() {
   return (
     <div className="App">
-      <img id="heroine" src="./Images/Heroine.png" alt="Hedona portrait" />
       <div className="main">
-        <div className="streamerLinks">
-          <div className="listenNow">LISTEN NOW:</div>
-          <img className="streamerLinks__icon" src="./Images/Spotify_Icon.png" alt="Spotify" />
-          <img className="streamerLinks__icon" src="./Images/Apple_Icon.png" alt="Apple Music" />
-          <img className="streamerLinks__icon" src="./Images/YT_Icon.png" alt="YouTube Music" />
-          <img className="streamerLinks__icon" src="./Images/Amazon_Icon.png" alt="Amazon Music" />
-          <img className="streamerLinks__icon" src="./Images/Soundcloud_Icon.png" alt="Soundcloud" />
-          <img className="streamerLinks__icon" src="./Images/Tidal_Icon.png" alt="Tidal" />
-        
-        
-        
-        </div>
-        <div className="bodyCopy">
-          <div className="bio">HEDONA crafts moody songs that question genres and push boundaries. Blending sounds and sonic textures that come from an eclectic mix of influences that include Björk, Kanye, YELLE, Sneaker Pimps, Portishead, Radiohead, and beyond, she’s successfully constructed a sound that’s distinctly HEDONA.
-            <p>Her first single, SWIMMING, is available to stream now.</p>
-          </div>
-          <div className="signup">GET THE LATEST NEWS, EXCLUSIVES, AND MORE:
-            <div className="signup__form">
-
+      <img id="heroine" src="./Images/Heroine.png" alt="Hedona portrait"/>
+      <img id="heroinePortrait" src="./Images/Heroine_portrait.png" alt="Hedona portrait" />
+        <div className="bodyContainer">
+          <div className="streamers">
+            <div className="streamers__listenNow">LISTEN NOW:</div>
+            <div className="streamers__links">
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/Spotify_Icon.png" alt="Spotify" /></a>
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/Apple_Icon.png" alt="Apple Music" /></a>
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/YT_Icon.png" alt="YouTube Music" /></a>
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/Amazon_Icon.png" alt="Amazon Music" /></a>
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/Soundcloud_Icon.png" alt="Soundcloud" /></a>
+            <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="streamers__links-icons" src="./Images/Tidal_Icon.png" alt="Tidal" /></a>
             </div>
           </div>
-          <div className="socials">
-            <img className="socials__icon" src="./Images/TikTok_Icon.png" alt="TikTok" />
-            <img className="socials__icon" src="./Images/Instagram_Icon.png" alt="Instagram" />
-            <img className="socials__icon" src="./Images/TikTok_Icon.png" alt="LinkedIn" />
-            <img className="socials__icon" src="./Images/Facebook_Icon.png" alt="Facebook" />
+          <div className="bodyCopy">
+            <div className="bodyCopy__bio">HEDONA crafts moody songs that question genres and push boundaries. Blending sounds and sonic textures that come from an eclectic mix of influences that include Björk, Kanye, YELLE, Sneaker Pimps, Portishead, Radiohead, and beyond, she’s successfully constructed a sound that’s distinctly HEDONA.
+              <p>Her first single, SWIMMING, is available to stream now.</p>
+            </div>
+            <div className="bodyCopy__signup">GET THE LATEST NEWS, EXCLUSIVES, AND MORE:
+                <Mailchimp action='https://gmail.us17.list-manage.com/subscribe/post?u=e33d309c6388144648843d6ae&amp;id=bf5dcdd7b6&amp;f_id=002546e0f0'
+                  fields={[
+                    {
+                      name: 'EMAIL',
+                      placeholder: 'YOUR EMAIL HERE',
+                      type: 'email',
+                      required: true
+                    }
+                  ]}  messages = {
+                    {
+                      sending: "Sending...",
+                      success: "Thank you for signing up!",
+                      error: "An unexpected internal error has occurred.",
+                      empty: "You must write an e-mail.",
+                      duplicate: "Too many sign up attempts for this email address",
+                      button: "SIGN UP"
+                    }
+                  }/>
+            </div>
+            <div className="bodyCopy__socials">
+              <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="bodyCopy__socials-icon" src="./Images/TikTok_Icon.png" alt="TikTok" /></a>
+              <a href="https://www.instagram.com/hedona.music/" target="_blank" rel="noreferrer"><img className="bodyCopy__socials-icon" src="./Images/Instagram_Icon.png" alt="Instagram" /></a>
+              <a href="https://soundcloud.com/user-112780443/tracks" target="_blank" rel="noreferrer"><img className="bodyCopy__socials-icon" src="./Images/Facebook_Icon.png" alt="Facebook" /></a>
+              <a href="https://www.linkedin.com/company/hedona-music/about/" target="_blank" rel="noreferrer"><img className="bodyCopy__socials-icon" src="./Images/LinkedIn_Icon.png" alt="LinkedIn" /></a>
+            </div>
           </div>
         </div>
       </div>
